7c535423fb70648952241189de0afeaaae07b047
rundekugel <rundekugel@users.noreply.github.com>
2022-01-29 22:33:16 +0100

    timer commit fix
    last commit wasn't complete - fixed

--- timer.py
+++ timer.py
@@ -2,27 +2,41 @@
 
 import machine
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 class Timer:
   ti=None
   cb=None
-  tid=None
+  cb_userinfo = None
+  callbackparamtype = "self"
+  timer_id=None
   wait=0
   verbosity=1
   running = 0
   intervall = None
   
-  def __init__(self, intervall_ms, callback=None, tid=-1):
-    self.tid=tid
-    self.ti= machine.Timer(tid)
+  def __init__(self, intervall_ms, callback=None, callbackparamtype="self", timer_id=-1):
+    """params: intervall in ms, optional callback, optional type of callback params: "no","self","user" """
+    self.timer_id=timer_id
+    self.ti= machine.Timer(timer_id)
     if callback:
       self.cb = callback
+      self.cbparamcount = callbackparamcount
+      if self.cbparamcount > 2:
+        raise Exception("max. 2 params allowed")
     self.setintervall(intervall_ms)
     
   def _cb(self,t=None):
     if self.cb and self.running:
-      self.cb(t)
+      if self.callbackparamtype == "user":
+        self.cb(self.cb_userinfo)
+      elif self.callbackparamtype == "self":
+        self.cb(self)
+      elif self.callbackparamtype == "no":
+        self.cb()
+      else:
+        if self.verbosity:
+          print("unknown callbackparamtype:" +str(self.callbackparamtype))
       
   def __del__(self):
     self.ti.deinit()
@@ -34,8 +48,11 @@ class Timer:
     self.ti.init(period=ms, callback=self._cb)
     self.running = 1
     
-  def setcallback(self, cb):
-    self._cb = cb
+  def setcallback(self, cb, callbackparamtype=None, userinfo=None):
+    self.cb = cb
+    if callbackparamtype:
+      self.callbackparamtype = callbackparamtype
+    self.cb_userinfo = userinfo
     
   def stop(self):
     self.running = 0
@@ -43,5 +60,9 @@ class Timer:
     self.running = 1
   def reset(self):
     self.setintervall(self.intervall)
+    
+  def info(self):
+    return {"Intervall":self.intervall, "Callback":self.cb, "Callback parameter type":self.callbackparamtype, 
+            "Userinfo":self.cb_userinfo, "Running":self.running, "verbosity":self.verbosity}
     
 #eof
